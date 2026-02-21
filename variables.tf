b7323fba5935f9ba078e9d3ce2eb81ff7585c658
Thai Truong <thai.truong@katalon.com>
2026-02-21 18:38:33 +0700

    Refactor to use more variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,41 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "ami_filter" {
+  description = "Name filter and owner filter for AMI"
+
+  type = object ({
+    name = string
+    owner = string
+  })
+
+  default = {
+    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631" # Bitnami
+  }
+}
+
+variable "environment" {
+  description = "Deployment environment"
+
+  type = object ({
+    name           = string
+    network_prefix = string    
+  })
+
+  default = {
+    name = "dev"
+    network_prefix = "10.0"
+  }
+}
+
+variable "min_size" {
+  description = "Minimum nunber of instances in the ASG"
+  default = 1
+}
+
+variable "max_size" {
+  description = "Maximum nunber of instances in the ASG"
+  default = 2
+}
